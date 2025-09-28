25ec1c52b0ca8e508d255b31539f07c3853c0447
BICHOTEE <emilio3547@outlook.es>
2025-09-28 15:09:06 -0600

    Initial Commit

---
+++ MeshRed/MeshRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshRedApp.swift
+//  MeshRed
+//
+//  Created by Emilio Contreras on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MeshRedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeshRedTests/MeshRedTests.swift
@@ -0,0 +1,16 @@
+//
+//  MeshRedTests.swift
+//  MeshRedTests
+//
+//  Created by Emilio Contreras on 28/09/25.
+//
+
+import Testing
+
+struct MeshRedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
